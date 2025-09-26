136ebfd4a6be507c879ac2010b6889cef789ab3b
Donald Raab <donraab@gmail.com>
2025-09-26 18:23:44 -0400

    Add List factories

--- src/test/java/refactortoec/FactoriesTest.java
+++ src/test/java/refactortoec/FactoriesTest.java
@@ -1,12 +1,16 @@
 package refactortoec;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.set.ImmutableSet;
 import org.eclipse.collections.api.set.MutableSet;
+import org.eclipse.collections.api.factory.Lists;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,6 +44,36 @@ public class FactoriesTest
         MutableSet<Set<String>> jdkSets =
                 Sets.mutable.of(jdkImmutableSet, jdkMutableSet);
         ecSets.cartesianProduct(jdkSets)
+                .forEach(pair -> assertEquals(pair.getOne(), pair.getTwo()));
+    }
+
+    @Test
+    public void listFactories()
+    {
+        // JDK Types
+        List<String> jdkMutableList =
+                new ArrayList<>(List.of("1", "2", "3"));
+        List<String> jdkImmutableList =
+                List.of("1", "2", "3");
+
+        // Drop-in replacements
+        List<String> ecMutableList1 =
+                Lists.mutable.of("1", "2", "3");
+        List<String> ecImmutableList1 =
+                Lists.immutable.of("1", "2", "3").castToList();
+
+        // Eclipse Collections Types
+        MutableList<String> ecMutableList2 =
+                Lists.mutable.of("1", "2", "3");
+        ImmutableList<String> ecImmutableList2 =
+                Lists.immutable.of("1", "2", "3");
+
+        // Testing JDK and Eclipse Collections Set Types for Equality
+        MutableSet<Iterable<String>>
+                ecLists = Sets.mutable.of(ecMutableList1, ecImmutableList1, ecMutableList2, ecImmutableList2);
+        MutableSet<List<String>> jdkLists =
+                Sets.mutable.of(jdkImmutableList, jdkMutableList);
+        ecLists.cartesianProduct(jdkLists)
                 .forEach(pair -> assertEquals(pair.getOne(), pair.getTwo()));
     }
 }
