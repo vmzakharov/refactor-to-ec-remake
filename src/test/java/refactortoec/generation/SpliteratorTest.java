4f29352211268db77e8991bfcb8e421d65d63021
Donald Raab <donraab@gmail.com>
2025-10-14 22:30:20 -0400

    New SpliteratorTest

--- src/test/java/refactortoec/generation/SpliteratorTest.java
+++ src/test/java/refactortoec/generation/SpliteratorTest.java
@@ -4,33 +4,49 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.eclipse.collections.impl.list.Interval;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpliteratorTest
 {
-    private final Interval interval = Interval.oneTo(1_000_000);
-    private final List<Integer> listN = List.copyOf(interval);
-    private final List<Integer> arrayList = new ArrayList<>(interval);
-    private final List<Integer> arraysAsList = Arrays.asList(interval.toArray());
-
     @Test
     public void listNSpliteratorType()
     {
-        assertEquals("RandomAccessSpliterator", listN.spliterator().getClass().getSimpleName());
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        assertEquals(
+                "RandomAccessSpliterator",
+                integers.spliterator().getClass().getSimpleName());
+    }
+
+    @Test
+    public void list12SpliteratorType()
+    {
+        List<Integer> list1Of12 = List.of(1);
+        assertEquals(
+                "",
+                list1Of12.spliterator().getClass().getSimpleName());
+        List<Integer> list2Of12 = List.of(1, 2);
+        assertEquals(
+                "RandomAccessSpliterator",
+                list2Of12.spliterator().getClass().getSimpleName());
     }
 
     @Test
     public void ArraysAsListSpliteratorType()
     {
-        assertEquals("ArraySpliterator", arraysAsList.spliterator().getClass().getSimpleName());
+        List<Integer> arraysAsList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        assertEquals(
+                "ArraySpliterator",
+                arraysAsList.spliterator().getClass().getSimpleName());
     }
 
     @Test
     public void ArrayListSpliteratorType()
     {
-        assertEquals("ArrayListSpliterator", arrayList.spliterator().getClass().getSimpleName());
+        List<Integer> arrayList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        assertEquals(
+                "ArrayListSpliterator",
+                arrayList.spliterator().getClass().getSimpleName());
     }
 }
