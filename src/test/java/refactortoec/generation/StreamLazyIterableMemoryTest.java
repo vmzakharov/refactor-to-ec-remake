4d0f3bfbf81da1b8012e82a2ac748c2e6efd0dee
Donald Raab <donraab@gmail.com>
2026-03-08 16:27:50 -0400

    Update memory tests for Stream and LazyIterable

--- src/test/java/refactortoec/generation/StreamLazyIterableMemoryTest.java
+++ src/test/java/refactortoec/generation/StreamLazyIterableMemoryTest.java
@@ -16,7 +16,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StreamLazyIterableMemoryTest
 {
     @Test
-    public void emptyArrayListAndStream()
+    public void emptyArrayListAndStreamFilter()
+    {
+        // An Empty ArrayList costs 40 bytes
+        // 24 bytes for ArrayList + 16 bytes for singleton Empty Array
+        assertEquals(
+                40, GraphLayout.parseInstance(new ArrayList<>()).totalSize());
+        assertEquals(
+                24, ClassLayout.parseInstance(new ArrayList<>()).instanceSize());
+
+        Stream<?> stream =
+                new ArrayList<>().stream()
+                        .filter(each -> true);
+
+        // An Empty Filter Stream costs 176 bytes (Includes empty ArrayList)
+        assertEquals(
+                176, GraphLayout.parseInstance(stream).totalSize());
+        System.out.println(GraphLayout.parseInstance(stream).toFootprint());
+    }
+
+    @Test
+    public void emptyFastListAndLazyIterableSelect()
+    {
+        // An Empty ArrayList costs 32 bytes
+        // 16 bytes for FastList + 16 bytes for singleton Empty Array
+        assertEquals(
+                32, GraphLayout.parseInstance(new FastList<>()).totalSize());
+        assertEquals(
+                16, ClassLayout.parseInstance(new FastList<>()).instanceSize());
+
+        LazyIterable<?> lazyIterable =
+                new FastList<>().asLazy()
+                        .select(each -> true);
+
+        // An Empty Select LazyIterable costs 56 bytes (Includes empty FastList)
+        assertEquals(
+                56, GraphLayout.parseInstance(lazyIterable).totalSize());
+        System.out.println(GraphLayout.parseInstance(lazyIterable).toFootprint());
+    }
+
+    @Test
+    public void emptyArrayListAndStreamFilterMap()
     {
         // An Empty ArrayList costs 40 bytes
         // 24 bytes for ArrayList + 16 bytes for singleton Empty Array
@@ -30,11 +70,13 @@ public class StreamLazyIterableMemoryTest
                         .filter(each -> true)
                         .map(each -> each);
 
-       System.out.println(GraphLayout.parseInstance(stream).toFootprint());
+        // An Empty Filter/Map Stream costs 240 bytes (Includes empty ArrayList)
+        assertEquals(
+                240, GraphLayout.parseInstance(stream).totalSize());
     }
 
     @Test
-    public void emptyFastListAndLazyIterable()
+    public void emptyFastListAndLazyIterableSelectCollect()
     {
         // An Empty ArrayList costs 32 bytes
         // 16 bytes for FastList + 16 bytes for singleton Empty Array
@@ -50,36 +92,8 @@ public class StreamLazyIterableMemoryTest
 
         // An Empty Select/Collect LazyIterable costs 80 bytes (Includes empty FastList)
         assertEquals(
-                120, GraphLayout.parseInstance(lazyIterable).totalSize());
+                80, GraphLayout.parseInstance(lazyIterable).totalSize());
         System.out.println(GraphLayout.parseInstance(lazyIterable).toFootprint());
-    }
-
-    @Test
-    public void emptyImmutableJDKListAndStream()
-    {
-        assertEquals(
-                32, GraphLayout.parseInstance(List.of()).totalSize());
-
-        Stream<?> stream =
-                List.of().stream()
-                        .filter(each -> true)
-                        .map(each -> each);
-        assertEquals(
-                240, GraphLayout.parseInstance(stream).totalSize());
-    }
-
-    @Test
-    public void emptyImmutableListAndLazyIterable()
-    {
-        assertEquals(
-                8, GraphLayout.parseInstance(Lists.immutable.empty()).totalSize());
-
-        LazyIterable<?> lazyIterable = Lists.immutable.empty().asLazy()
-                .select(each -> true)
-                .collect(each -> each);
-
-        assertEquals(
-                56, GraphLayout.parseInstance(lazyIterable).totalSize());
     }
 }
 
